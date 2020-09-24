e766aa155449033bae501b4432240d1a8b1cdee4
DavidRMorphew <morphewd@umich.edu>
2020-09-24 18:41:13 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
     end
 
     def add_student(name, grade)
-        if @roster == {} || !@roster.keys.include?(grade)
+        if !@roster.keys.include?(grade)
             @roster[grade] = [name]
         else 
             @roster[grade]<<name
