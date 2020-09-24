eb3e4f57dac1b8a44e846bf4ca85c3e0bff6773f
DavidRMorphew <morphewd@umich.edu>
2020-09-24 09:06:19 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+require 'pry'
+class School
+
+    attr_reader :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster == {} || !@roster.keys.include?(grade)
+            @roster[grade] = [name]
+        else 
+            @roster[grade]<<name
+        end
+    end
+end
